5544f8392312a5f3863f1b815baa0953dc723516
Yedianc <emperorzanji@gmail.com>
2023-11-17 21:33:25 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
+
+const Square = ({ value, onClick }) => (
+  <button className="square" onClick={(e) => { e.preventDefault(); onClick(); }}>
+    {value}
+  </button>
+);
+
+
+const Board = ({ onRestart }) => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
+
+  const handleClick = (index) => {
+    if (winner || squares[index]) {
+      return;
+    }
+
+    const newSquares = squares.slice();
+    newSquares[index] = xIsNext ? 'X' : 'O';
+
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index) => (
+    <Square value={squares[index]} onClick={() => handleClick(index)} />
+  );
+
+  const status = winner ? `Winner: ${winner}` : squares.every(Boolean) ? 'It\'s a Tie!' : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      {(winner || squares.every(Boolean)) && (
+        <button className="restart-button" onClick={onRestart}>
+          Restart
+        </button>
+      )}
+    </div>
+  );
+};
+
+const App = () => {
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const handleStart = () => {
+    setGameStarted(true);
+  };
+
+  const handleRestart = () => {
+    setGameStarted(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Tic Tac Toe</h1>
+        {!gameStarted ? (
+          <button className="start-button" onClick={handleStart}>
+            Start
+          </button>
+        ) : (
+          <Board onRestart={handleRestart} />
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
